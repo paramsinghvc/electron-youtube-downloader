95291cd8cda1f3b845db0b99583355d2871aa7f0
Parminder Singh <parminder.s@flipkart.com>
2018-12-21 17:06:40 +0530

    readme

--- README.md
+++ README.md
@@ -2,9 +2,15 @@
 
 > An electron-vue project
 
+#### Run Server
+
+```node
+node server
+```
+
 #### Build Setup
 
-``` bash
+```bash
 # install dependencies
 npm install
 
@@ -22,7 +28,3 @@ npm test
 npm run lint
 
 ```
-
----
-
-This project was generated with [electron-vue](https://github.com/SimulatedGREG/electron-vue)@[8fae476](https://github.com/SimulatedGREG/electron-vue/tree/8fae4763e9d225d3691b627e83b9e09b56f6c935) using [vue-cli](https://github.com/vuejs/vue-cli). Documentation about the original structure can be found [here](https://simulatedgreg.gitbooks.io/electron-vue/content/index.html).
--- src/main/index.js
+++ src/main/index.js
@@ -1,54 +1,96 @@
-'use strict'
+"use strict";
 
-import { app, BrowserWindow, globalShortcut } from 'electron'
+import { app, BrowserWindow, globalShortcut, Menu } from "electron";
 
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
-if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+if (process.env.NODE_ENV !== "development") {
+	global.__static = require("path")
+		.join(__dirname, "/static")
+		.replace(/\\/g, "\\\\");
 }
 
-let mainWindow
-const winURL = process.env.NODE_ENV === 'development'
-  ? `http://localhost:9080`
-  : `file://${__dirname}/index.html`
+let mainWindow;
+const winURL =
+	process.env.NODE_ENV === "development"
+		? `http://localhost:9080`
+		: `file://${__dirname}/index.html`;
 
-function createWindow () {
-  /**
-   * Initial window options
-   */
-  mainWindow = new BrowserWindow({
-    height: 900,
-    useContentSize: true,
-    width: 1800
-  })
-
-  mainWindow.loadURL(winURL)
-
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
-
-  globalShortcut.register("MediaPlayPause", () => {
-		console.log("MediaPlayPause is pressed");
+function createWindow() {
+	/**
+	 * Initial window options
+	 */
+	mainWindow = new BrowserWindow({
+		height: 900,
+		useContentSize: true,
+		width: 1800
 	});
+
+	mainWindow.loadURL(winURL);
+
+	mainWindow.on("closed", () => {
+		mainWindow = null;
+	});
+
+	// Create the Application's main menu
+	var template = [
+		{
+			label: "Application",
+			submenu: [
+				{
+					label: "About Application",
+					selector: "orderFrontStandardAboutPanel:"
+				},
+				{ type: "separator" },
+				{
+					label: "Quit",
+					accelerator: "Command+Q",
+					click: function() {
+						app.quit();
+					}
+				}
+			]
+		},
+		{
+			label: "Edit",
+			submenu: [
+				{ label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+				{ label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+				{ type: "separator" },
+				{ label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+				{ label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+				{ label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+				{
+					label: "Select All",
+					accelerator: "CmdOrCtrl+A",
+					selector: "selectAll:"
+				}
+			]
+		}
+	];
+
+	Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+
+	// globalShortcut.register("MediaPlayPause", () => {
+	// 	console.log("MediaPlayPause is pressed");
+	// });
 }
 
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
+});
 
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
+app.on("activate", () => {
+	if (mainWindow === null) {
+		createWindow();
+	}
+});
 
 /**
  * Auto Updater
